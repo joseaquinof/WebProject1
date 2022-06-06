e1d33499df0918a3b3a0b44f7aa7b43601a4e5c7
jared <jared@DESKTOP-OV7TPP6>
2022-06-06 12:41:47 -0600

    Add project files.

---
+++ WebProject1/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebProject1.Models;
+using Newtonsoft.Json;
+
+namespace WebProject1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> IndexAsync()
+        {
+            //List<Root> members = new List<Root>();
+            Root members = null;
+
+            //for (int i = 0; i < 10; i++)
+            //{
+                using (var client = new HttpClient())
+                {
+                    var result = await client.GetAsync("https://randomuser.me/api/?results=100");
+
+                    if (result.IsSuccessStatusCode)
+                    {
+                        var content = await result.Content.ReadAsStringAsync();
+                        members = JsonConvert.DeserializeObject<Root>(content);
+                        //members.Add(member);
+                    }
+                    else //web api sent error response 
+                    {
+                        //log response status here..
+
+                        members = null;
+
+                        ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                    }
+                }
+            //}
+            return View(members);
+        }
+
+        public ActionResult Detail(SelectedMemberModel member)
+        {
+            //Root selectedMember = members.Where(p=>p.info.seed == id).Single();
+            return View(member);
+
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebProject1/Models/SelectedMemberModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebProject1.Models
+{
+    public class SelectedMemberModel
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostCode { get; set; }
+        public string Email { get; set; }
+        
+        public string Dob { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public string PhotoLarge { get; set; }
+    }
+}
